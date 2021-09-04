6f80503cf7a27863f98bd2e9fc2c7669bd743d5b
MrClottom <philippe@dumo.net>
2021-09-04 16:47:57 +0200

    Remove metadata verification endpoints

--- src/endpoints.js
+++ src/endpoints.js
@@ -2,10 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { verify, web3, verifier, sale } = require('./web3.js')
 const { ApiError, createHandler } = require('./api.js')
-const allTokenURIs = require('./token-uris.env.js')
 const directWhitelist = require('./whitelist.env.js')
 
-const METADATA_WAIT = parseInt(process.env.METADATA_WAIT)
 const whitelist = new Set(directWhitelist.map((address) => address.toLowerCase()))
 
 router.post(
@@ -39,49 +37,6 @@ router.get(
 router.get(
   '/sale-contract',
   createHandler(async () => ({ saleAddress: process.env.SALE_ADDRESS }))
-)
-
-router.post(
-  '/get-metadata-proof',
-  createHandler(async ({ body }) => {
-    const rawTokenIds = body?.tokenIds
-    if (!(rawTokenIds instanceof Array)) {
-      throw new ApiError(400, 'tokenIds field required and must be an array')
-    }
-    const tokenIds = rawTokenIds.map((tokenId) => parseInt(tokenId))
-    if (tokenIds.some((tokenId) => isNaN(tokenId))) {
-      throw new ApiError(400, 'tokenIds must be numbers or numbers or strings')
-    }
-    if (tokenIds.length == 0) {
-      throw new ApiError(400, 'must provide at least 1 tokenId')
-    }
-    const lowestTokenId = tokenIds.reduce((a, b) => Math.min(a, b))
-    if (lowestTokenId < 0) {
-      throw new ApiError(400, 'tokenIds start at 0')
-    }
-    await sale.fetchPastBuys()
-    const latestBlock = sale.nextUpdateBlock - 1
-    const earliestBlock = latestBlock - METADATA_WAIT
-    const acceptedBuys = sale.findLastBuy(earliestBlock)
-    const highestTokenId = tokenIds.reduce((a, b) => Math.max(a, b), -1)
-    console.log('acceptedBuys: ', acceptedBuys)
-    console.log('highestTokenId: ', highestTokenId)
-    if (acceptedBuys === null) {
-      throw new ApiError(401, 'no tokens yet available')
-    }
-    if (highestTokenId >= acceptedBuys.total) {
-      throw new ApiError(
-        401,
-        `tokens not yet available, latest available: #${acceptedBuys.total - 1}`
-      )
-    }
-    const tokenURIs = tokenIds.map((tokenId) => allTokenURIs[tokenId])
-    const signature = await verify.metadata(tokenIds, tokenURIs)
-    return {
-      tokenURIs,
-      signature
-    }
-  })
 )
 
 router.post(
--- src/web3.js
+++ src/web3.js
@@ -6,62 +6,12 @@ const web3 = new Web3(
 )
 
 const sale = new web3.eth.Contract(SaleContractAbi, process.env.SALE_ADDRESS)
-sale.pastBuys = []
-sale.nextUpdateBlock = 'earliest'
-
-sale.fetchPastBuys = async () => {
-  const { number: block } = await web3.eth.getBlock('latest')
-  if (block < sale.nextUpdateBlock) return
-  const events = await sale.getPastEvents('Buy', {
-    fromBlock: sale.nextUpdateBlock,
-    toBlock: block
-  })
-  const buys = events.map(({ blockNumber, returnValues }) => ({
-    block: blockNumber,
-    amount: parseInt(returnValues.amount)
-  }))
-  const accBuys = []
-  const pastBuys = sale.pastBuys.length
-  let total = pastBuys > 0 ? sale.pastBuys[pastBuys - 1].total : 0
-  for (const { block, amount } of buys) {
-    total += amount
-    accBuys.push({ block, total })
-  }
-  sale.pastBuys = sale.pastBuys.concat(accBuys)
-  sale.nextUpdateBlock = block + 1
-}
-
-sale.findLastBuy = (beforeBlock) => {
-  let left = 0
-  const buys = sale.pastBuys.length
-  let right = buys - 1
-
-  const maxSearch = Math.floor(Math.log(buys) / Math.log(2)) + 1
-  for (let i = 0; i < maxSearch; i++) {
-    const mid = Math.floor((left + right) / 2)
-    const buy = sale.pastBuys[mid]
-    if (buy.block == beforeBlock) return buy
-    if (buy.block > beforeBlock) right = mid - 1
-    else left = mid + 1
-
-    if (right < left) {
-      if (right == -1) return null
-      return sale.pastBuys[right]
-    }
-  }
-
-  throw new Error('not found')
-}
-
-sale.fetchPastBuys()
-
 const verifier = web3.eth.accounts.wallet.add(process.env.VERIFIER_PRIV_KEY)
 
 const keccak256 = (value) => web3.utils.soliditySha3({ type: 'string', value })
 const constants = {
   DS_IS_WHITELISTED: keccak256('trippy-nfts.access.is-whitelisted(address)'),
-  DS_CAPTCHA_SOLVED: keccak256('trippy-nfts.access.captcha-solved(address)'),
-  DS_VALID_METADATA: keccak256('trippy-nfts.verif.valid-metadata(uint256,string)')
+  DS_CAPTCHA_SOLVED: keccak256('trippy-nfts.access.captcha-solved(address)')
 }
 
 const adjustSigV = (sig) => {
@@ -75,8 +25,8 @@ const adjustSigV = (sig) => {
 
 const signData = async (encodedData) => {
   const hash = web3.utils.soliditySha3(encodedData)
-  const directSig = await web3.eth.sign(hash, verifier.address)
-  return adjustSigV(directSig)
+  const { signature } = await verifier.sign(hash)
+  return adjustSigV(signature)
 }
 
 const createAccountVerify = (domainSeparator) => async (account) => {
@@ -86,13 +36,6 @@ const createAccountVerify = (domainSeparator) => async (account) => {
 
 const whitelist = createAccountVerify(constants.DS_IS_WHITELISTED)
 const captcha = createAccountVerify(constants.DS_CAPTCHA_SOLVED)
-const metadata = async (tokenIds, tokenURIs) => {
-  const encoded = web3.eth.abi.encodeParameters(
-    ['bytes32', 'uint256[]', 'string[]'],
-    [constants.DS_VALID_METADATA, tokenIds, tokenURIs]
-  )
-  return await signData(encoded)
-}
 
 module.exports = {
   web3,
@@ -100,7 +43,6 @@ module.exports = {
   verifier,
   verify: {
     whitelist,
-    captcha,
-    metadata
+    captcha
   }
 }
